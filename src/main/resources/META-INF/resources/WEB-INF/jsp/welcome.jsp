4ae61315fcb4dfa8458a3f87488c62458e373919
Goes <goesmodul@gmail.com>
2023-09-06 20:17:15 +0700

    First Commit

---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,9 @@
+<%@ include file="common/header.jspf" %>
+<%@ include file="common/navigation.jspf" %>
+	<div class="container">
+		<h1>Welcome Name: ${name}</h1>
+	
+		<a href="list-todos">Manage</a> your todos
+	</div>
+<%@ include file="common/script.jspf" %>
+<%@ include file="common/footer.jspf" %>
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'test', 'Get AWS Certified', CURRENT_DATE(), false);
+
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'test', 'Get Azure Certified', CURRENT_DATE(), false);
+
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'test', 'Get GCP Certified', CURRENT_DATE(), false);
+
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'test', 'Learn DevOps', CURRENT_DATE(), false);
